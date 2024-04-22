35c305dd0f23d288556599f21c60a76dffee47b7
Yaroslav Matusevich <yari.matusevich@Gmail.com>
2024-04-21 23:38:52 -0400

    Added UserAccount class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        UserAccount newUser =
+                new UserAccount(
+                        "george24", "George King",
+                        "42 Bank Ave, New York, NY", "password", 42.50
+                );
+
+        System.out.println(newUser);
     }
 }
---
+++ src/UserAccount.java
@@ -0,0 +1,51 @@
+public class UserAccount {
+    private final String id;
+    private final String name;
+    private String address;
+    private String password;
+    private double balance;
+
+    UserAccount(String id, String name, String address, String password, double balance) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.password = password;
+        this.balance = balance;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String toString() {
+        return name + ", " + address + ", " + balance;
+    }
+}
