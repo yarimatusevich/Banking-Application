75a03463ed5e3bdaf0ea755ce0584ddd7aa8db05
Yaroslav Matusevich <yari.matusevich@Gmail.com>
2024-04-22 00:00:42 -0400

    Added AccountDataBase.java Added TransactionManager.java

---
+++ src/AccountDataBase.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+public class AccountDataBase {
+
+    public static void storeAccount() {
+
+    }
+
+    public static void removeAccount() {
+
+    }
+}
--- src/AccountManager.java
+++ src/AccountManager.java
@@ -1,3 +1,17 @@
 public class AccountManager {
+    public static Account createAccount() {
 
+    }
+
+    public static void deleteAccount() {
+
+    }
+
+    public static void changeAddress() {
+
+    }
+
+    public static void changePassword() {
+
+    }
 }
