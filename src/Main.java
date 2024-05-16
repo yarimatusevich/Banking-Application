eebf48d9b3cb94e45244032c61d2f98136477284
Yaroslav Matusevich <yari.matusevich@Gmail.com>
2024-05-16 18:56:50 -0400

    Log in is functioning

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,17 @@ public class Main {
     static HashMap<String, Account> accountDataBase = new HashMap<>();
 
     public static void main(String[] args) {
-        test(accountDataBase);
+        test();
     }
 
-    private static void test(HashMap<String, Account> accountDataBase) {
+    private static void test() {
         Scanner scanner = new Scanner(System.in);
 
+        String welcomeMessage =
+                "Welcome! Do you wish to create a new account (N), log into an existing account (L), or quit (Q)?";
+
         System.out.println(
-                "Welcome! Do you wish to create a new account (N)," +
-                        " log into an existing account (L), or quit (Q)?"
+                welcomeMessage
         );
 
         while (scanner.hasNext()) {
@@ -27,29 +29,53 @@ public class Main {
                 }
                 case "L" -> {
                     // log in
+                    System.out.println("Enter your user id");
+                    String id = scanner.nextLine();
+
+                    Account current = AccountDataBaseUtilities.getAccount(id);
+
+                    // Checking if user with this id exists
+                    if (current != null) {
+                        System.out.println("Enter your password");
+                        String password = scanner.nextLine();
+                        String correctPassword = current.getPassword();
+
+                        if (password.equals(correctPassword)) {
+                            AccountManager.logIn(id, password);
+                        } else {
+                            System.out.println("Wrong password.");
+                            System.out.println(welcomeMessage);
+                        }
+
+                    } else {
+                        System.out.println("Invalid user id.");
+                        System.out.println(welcomeMessage);
+                    }
                 }
                 case "N" -> {
                     // Printing prompts and gathering responses
                     System.out.println("Create a user id");
                     String id = scanner.nextLine();
 
+                    // Prompting for password
                     System.out.println("Create new password");
                     String password = scanner.nextLine();
 
+                    // Prompting for user's full name
                     System.out.println("Enter your full name");
                     String name = scanner.nextLine();
 
+                    // Prompting for user address
                     System.out.println("Enter your address");
                     String address = scanner.nextLine();
 
+                    // Prompting users for initial deposit
                     System.out.println("Enter amount of initial deposit");
                     double initialDeposit = scanner.nextDouble();
 
+                    // Creating account using info from responses
                     AccountManager.createAccount(id, password, name, address, initialDeposit);
                     System.out.println("Account successfully created");
-
-                    Account current = accountDataBase.get(id);
-                    System.out.println(current);
                 }
                 default -> System.out.println("Invalid input");
             }
